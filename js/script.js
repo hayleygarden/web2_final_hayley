0dac668490b7b4ab56a889a3670998bfd761927f
Lauren Gibbons <lauren@Laurens-MacBook-Pro-2.local>
2019-05-03 18:33:27 -0400

    graph updates

--- js/script.js
+++ js/script.js
@@ -7,16 +7,18 @@ var myData = [
 	{"name": "Skool Luv Affair", "date": "February 12th, 2014", "title": "Boy in Luv", "sale": 5, "color": "#6d2428"},
 	{"name": "Dark n Wild", "date": "August 19th, 2014", "title": "Danger", "sale": 16, "color": "#304833"},
 	{"name": "HYYH: Pt 1", "date": "April 29th, 2015", "title": "I Need U", "sale": 55, "color": "#fdf4f3"},
-	{"name": "HYYH: Pt 2", "date": "November 30th, 2015", "title": "Run", "description": 86, "color": "#77cdec"},
+	{"name": "HYYH: Pt 2", "date": "November 30th, 2015", "title": "Run", "sale": 86, "color": "#77cdec"},
 	{"name": "HYYH: Young Forever", "date": "May 2, 2016", "title": "Fire, Save Me", "sale": 164, "color": "#f1f6ad"},
 	{"name": "Wings", "date": "October 10th, 2016", "title": "Blood, Sweat, and Tears", "sale": 347, "color": "#111111"},
 	{"name": "You Never Walk Alone", "date": "February 13th, 2017", "title": "Spring Day, Not Today", "sale": 373, "color": "#89cfc3"},
-	{"name": "Love Yourself: Her", "date": "September 18th, 2017", "title": "DNA", "description": 750, "color": "#f4f7fd"},
+	{"name": "Love Yourself: Her", "date": "September 18th, 2017", "title": "DNA", "sale": 750, "color": "#f4f7fd"},
 	{"name": "Love Yourself: Tear", "date": "May 18th, 2018", "title": "Fake Love", "sale": 1000, "color": "#161824"},
 	{"name": "Love Yourself: Answer", "date": "August 24th, 2018", "title": "Idol", "sale": 860, "color": "#f3aac7"},
 	{"name": "Map of the Soul: Persona", "date": "April 12th, 2019", "title": "Boy With Luv", "sale": 2100, "color": "#fe769c"}
 
 ];
+
+var myData2 = [ 5, 16, 55, 86, 164, 347, 373, 759, 1000, 860, 2100];
 
 //LG check if chart in view
 function isScrolledIntoView(el) {
@@ -37,8 +39,9 @@ window.addEventListener('scroll', function (event) {
 	}
 }, false);
 
-var width = 600;
-var height = 500;
+var margin = {top: 20, right: 20, bottom: 30, left: 40}
+var width = 700 - margin.left - margin.right;
+var height = 600 - margin.top - margin.bottom;
 var animateDuration = 700;
 var animateDelay = 30;
 var tooltip = d3.select("body").append("div")
@@ -50,13 +53,13 @@ var tooltip = d3.select("body").append("div")
 	.style("opacity", "0")
 
 var yScale = d3.scaleLinear()
-	.domain([0, d3.max(100)])
-	.range([0, height]);
+	.domain([0, d3.max(myData, function(d) { return d.sale; })])
+	.range([height, 0]);
 
 var xScale = d3.scaleBand()
 	.domain(d3.range(0, myData.length))
 	.range([0, width])
-	.padding(.2);
+	.padding(.1);
 
 var colors = d3.scaleLinear()
 	.domain([0, myData.length])
@@ -66,23 +69,28 @@ var tip = d3.tip()
 	.attr('class', 'tooltip')
 	.offset([0, 2])
 	.html(function(d) {
-		return "<strong>" + d + "</strong>";
+		return "<em>" + d.title + "</em><br><strong>" + d.sale + "</strong>";
 });
 
-tip.direction('e')
+tip.direction('e');
 
-myChart = d3.select("#chart").append("svg")
+var yAxis = d3.axisRight(yScale)
+		.ticks(4);
+
+var xAxis = d3.axisBottom(xScale);
+
+var myChart = d3.select("#chart").append("svg")
 	.attr("width", width)
 	.attr("height", height)
-	.style("background", "#ffd5c7", "#f9b9db")
 	.selectAll("rect")
+	// .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 	.data(myData)
 	.enter().append("rect")
-	.style("fill", function(d){return myData.color;})
+	.style("fill", (d, i) => d.color)
 	.attr("width", xScale.bandwidth())
-	.attr("height", 0)
+	.attr("height", height)
 	.attr("x", (d, i) => xScale(i))
-	.attr("y", height)
+	.attr("y", 0)
 
 	.on('mouseover', function(d) {
         tip.attr('class', 'tooltip animate').show(d)
@@ -94,10 +102,10 @@ myChart = d3.select("#chart").append("svg")
 	
 myChart.transition()
 	.attr("height", function (d) {
-		return yScale(d);
+		return height - yScale(d.sale);
 	})
 	.attr("y", function (d) {
-		return height - yScale(d);
+		return yScale(d.sale);
 	})
 	.duration(animateDuration)
 	.delay(function (d, i) {
@@ -106,3 +114,11 @@ myChart.transition()
 	.ease(d3.easeElastic);
 
 myChart.call(tip);
+
+var svg = d3.select("svg");
+
+svg.append("g")
+		.attr("class", "axis y")
+		// .attr("transform", "translate(-100,0)")
+		.style("fill", "black")
+		.call(yAxis);
